960429f119bc33a6fa2d72046101cd08531fb6d3
Lunix-420 <76759448+Lunix-420@users.noreply.github.com>
2024-01-21 21:45:32 +0100

    Improved thread safety and error handling

--- src/utility/RingBuffer.h
+++ src/utility/RingBuffer.h
@@ -11,44 +11,62 @@
     audio data. It supports reading and writing audio data to and from the
     buffer.
 
-    @tparam T The type of audio data stored in the buffer.
+    @tparam SampleType The type of audio data stored in the buffer.
 */
-template<typename T>
+template<typename SampleType>
 class RingBuffer
 {
 public:
   //============================================================================
   /**
-      Constructs a RingBuffer object with the specified number of channels and
-     samples.
+      Constructs a RingBuffer object with the specified number of channels an
+      samples.
 
       @param numChannelsToAllocate The number of channels to allocate in the
-     buffer.
+      buffer.
       @param numSamplesToAllocate The number of samples to allocate in the
-     buffer.
+      buffer.
   */
   RingBuffer(const int numChannelsToAllocate, const int numSamplesToAllocate)
     : bufferSize(numSamplesToAllocate)
     , numChannels(numChannelsToAllocate)
     , position(0)
-    , buffer(std::make_unique<AudioBuffer<T>>(numChannelsToAllocate,
-                                              numSamplesToAllocate))
+    , buffer(std::make_unique<AudioBuffer<SampleType>>(numChannelsToAllocate,
+                                                       numSamplesToAllocate))
   {
   }
+  //============================================================================
+  /**
+      An enum class representing the result of an operation.
+
+      The OperationResult enum class represents the result of an operation.
+      It is used to indicate whether an operation was successful or not.
+
+      @see read
+      @see write
+  */
+  enum class OperationResult
+  {
+    Success,           /**< The operation was successful. */
+    ErrorSizeMismatch, /**< The operation failed due to a size mismatch. */
+  };
   //============================================================================
   /**
       Reads audio data from the buffer into the target buffer.
 
       @param targetBuffer The buffer to copy the audio data into.
   */
-  void read(AudioBuffer<T>& targetBuffer) const noexcept
+  const OperationResult read(
+    AudioBuffer<SampleType>& targetBuffer) const noexcept
   {
     // If targetBuffer is smaller than the RingBuffer we need to return early
     if (targetBuffer.getNumChannels() < numChannels ||
         targetBuffer.getNumSamples() < bufferSize) {
-      jassertfalse; // Throw an exception here
-      return;
+      jassertfalse; // Juce does not support exceptions, so we assert instead
+      return OperationResult::ErrorSizeMismatch;
     }
+    // We need to lock the buffer for reading to ensure thread safety
+    lock.enterRead();
     // Caching the atomic should be cheaper than calling get() every iteration
     const int startingPosition = position.get();
     // Iterate the samples first to improve thread safety
@@ -62,6 +80,10 @@ public:
           channel, sample, *buffer, channel, ringPosition, 1);
       }
     }
+    // We need to unlock the buffer after reading to ensure thread safety
+    lock.exitRead();
+    // We return the result of the operation
+    return OperationResult::Success;
   }
   //============================================================================
   /**
@@ -69,14 +91,17 @@ public:
 
       @param bufferToWrite The buffer containing the audio data to write.
   */
-  void write(const AudioBuffer<T>& bufferToWrite) noexcept
+  const OperationResult write(
+    const AudioBuffer<SampleType>& bufferToWrite) noexcept
   {
     // If bufferToWrite is larger than the RingBuffer we need to return early
     if (bufferToWrite.getNumChannels() > numChannels ||
         bufferToWrite.getNumSamples() > bufferSize) {
-      jassertfalse; // Throw an exception here
-      return;
+      jassertfalse; // Juce does not support exceptions, so we assert instead
+      return OperationResult::ErrorSizeMismatch;
     }
+    // We need to lock the buffer for writing to ensure thread safety
+    lock.enterWrite();
     // Caching the atomic should be cheaper than calling get() every iteration
     const int startingPosition = position.get();
     // Iterate the samples first to improve thread safety
@@ -92,11 +117,16 @@ public:
     }
     // We update the position for the readers after we've written the buffer
     position.set((startingPosition + bufferSize) % bufferSize);
+    // We need to unlock the buffer after writing to ensure thread safety
+    lock.exitWrite();
+    // We return the result of the operation
+    return OperationResult::Success;
   }
   //============================================================================
 private:
-  const int bufferSize;                   // Number of samples
-  const int numChannels;                  // Number of channels
-  juce::Atomic<int> position;             // Current starting position
-  std::unique_ptr<AudioBuffer<T>> buffer; // Buffer co
+  const int bufferSize;                            // Number of samples
+  const int numChannels;                           // Number of channels
+  juce::Atomic<int> position;                      // Current starting position
+  std::unique_ptr<AudioBuffer<SampleType>> buffer; // Buffer to store audio data
+  juce::ReadWriteLock lock;                        // Lock for thread safety
 };
--- tests/RingBufferTest.cpp
+++ tests/RingBufferTest.cpp
@@ -23,18 +23,27 @@ generateTestBuffer(int numChannels, int numSamples)
 // Unit Tests
 TEST(RingBuffer, read_write_correctness)
 {
-  const auto testBuffer = generateTestBuffer(7, 2048);
-
-  RingBuffer<float> ringBuffer(7, 2048);
-  ringBuffer.write(testBuffer);
-
-  juce::AudioBuffer<float> readBuffer(7, 2048);
-  ringBuffer.read(readBuffer);
-
-  for (int channel = 0; channel < 7; ++channel) {
-    for (int sample = 0; sample < 2048; ++sample) {
-      EXPECT_EQ(testBuffer.getSample(channel, sample),
-                readBuffer.getSample(channel, sample));
+  // Maximum number of channels and buffer size to test
+  const int maxNumChannels = 9;
+  const int maxBufferSize = 1024;
+  // Test all possible combinations of channels and buffer sizes
+  for (int numChannels = 1; numChannels < maxNumChannels; ++numChannels) {
+    for (int bufferSize = 1; bufferSize < maxBufferSize; ++bufferSize) {
+      // Generate a test data buffer
+      const auto testBuffer = generateTestBuffer(numChannels, bufferSize);
+      // Create a ring buffer and write the test buffer to it
+      RingBuffer<float> ringBuffer(numChannels, bufferSize);
+      ringBuffer.write(testBuffer);
+      // Create a buffer to read the ring buffer into
+      juce::AudioBuffer<float> readBuffer(numChannels, bufferSize);
+      ringBuffer.read(readBuffer);
+      // Compare the test buffer to the read buffer
+      for (int channel = 0; channel < numChannels; ++channel) {
+        for (int sample = 0; sample < bufferSize; ++sample) {
+          EXPECT_EQ(testBuffer.getSample(channel, sample),
+                    readBuffer.getSample(channel, sample));
+        }
+      }
     }
   }
 }
