cca7cadfdc7d5c005c7cd6361fa3147a9da78437
Lunix-420 <davidhess336@gmx.de>
2026-04-13 10:49:35 +0200

    fix: correct dimensions for OscilloscopePanel in PluginEditor constructor

--- src/app/PluginEditor.cpp
+++ src/app/PluginEditor.cpp
@@ -6,8 +6,8 @@ PluginEditor::PluginEditor(PluginProcessor& p)
   : dmt::app::AbstractPluginEditor(
       p,
       "Oscilloscope",
-      800,
-      400,
+      700,
+      325,
       [&p](dmt::gui::window::Layout& layout) {
         layout.addPanel<dmt::gui::panel::OscilloscopePanel<float>>(
           0, 0, 1, 1, p.apvts, p.oscilloscopeBuffer);
